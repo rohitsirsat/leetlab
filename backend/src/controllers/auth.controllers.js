528523c1beb1c7df43e04fd26700c27662d8cc09
rohit <rohitsirsat855@gmail.com>
2025-05-16 23:00:58 +0530

    std the auth

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -2,150 +2,123 @@ import bcrypt from "bcryptjs";
 import { db } from "../libs/db.js";
 import { UserRole } from "../generated/prisma/index.js";
 import jwt from "jsonwebtoken";
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
 
-const register = async (req, res) => {
+const register = asyncHandler(async (req, res) => {
   const { email, password, name } = req.body;
 
-  try {
-    const existingUser = await db.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  const existingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if (existingUser) {
-      return res.status(400).json({
-        error: "User already exists",
-      });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await db.user.create({
-      data: {
-        email,
-        password: hashedPassword,
-        name,
-        role: UserRole.USER,
-      },
-    });
-
-    const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV !== "development",
-      maxAge: 1000 * 60 * 60 * 24 * 7,
-    });
-
-    res.status(201).json({
-      message: "User created successfully",
-      user: {
-        id: newUser.id,
-        email: newUser.email,
-        name: newUser.name,
-        role: newUser.role,
-        image: newUser.image,
-      },
-    });
-  } catch (error) {
-    console.log("Error creating user: ", error);
-    res.status(500).json({
-      error: "Error creating user",
-    });
+  if (existingUser) {
+    throw new ApiError(400, "user already exists");
   }
-};
 
-const login = async (req, res) => {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const newUser = await db.user.create({
+    data: {
+      email,
+      password: hashedPassword,
+      name,
+      role: UserRole.USER,
+    },
+  });
+
+  const token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_SECRET_Expiry,
+  });
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+  };
+
+  return res
+    .status(200)
+    .cookie("jwt", token, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: {
+            id: newUser.id,
+            email: newUser.email,
+            name: newUser.name,
+            role: newUser.role,
+            imagez: newUser.image,
+          },
+        },
+        "User register successfully",
+      ),
+    );
+});
+
+const login = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  try {
-    const user = await db.user.findUnique({
-      where: {
-        email,
-      },
-    });
+  const user = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-    if (!user) {
-      return res.status(401).json({
-        error: "User not found",
-      });
-    }
-
-    const isMatch = await bcrypt.compare(password, user.password);
-
-    if (!isMatch) {
-      return res.status(401).json({
-        error: "Invalid credentials",
-      });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
-    });
-
-    res.cookie("jwt", token, {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV !== "development",
-      maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-    });
-
-    res.status(200).json({
-      success: true,
-      message: "User Logged in successfully",
-      user: {
-        id: user.id,
-        email: user.email,
-        name: user.name,
-        role: user.role,
-        image: user.image,
-      },
-    });
-  } catch (error) {
-    console.error("Error creating user:", error);
-    res.status(500).json({
-      error: "Error logging in user",
-    });
+  if (!user) {
+    throw new ApiError(401, "User not found");
   }
-};
 
-const logout = async (req, res) => {
-  try {
-    res.clearCookie("jwt", {
-      httpOnly: true,
-      sameSite: "strict",
-      secure: process.env.NODE_ENV !== "development",
-    });
+  const isMatch = await bcrypt.compare(password, user.password);
 
-    res.status(200).json({
-      success: true,
-      message: "User logged out successfully",
-    });
-  } catch (error) {
-    console.error("Error logging out user:", error);
-    res.status(500).json({
-      error: "Error logging out user",
-    });
+  if (!isMatch) {
+    throw new ApiError(401, "Invalid credentials");
   }
-};
 
-const getProfile = async (req, res) => {
-  try {
-    res.status(200).json({
-      success: true,
-      message: "User authenticated successfully",
-      user: req.user,
-    });
-  } catch (error) {
-    console.error("Error checking user:", error);
-    res.status(500).json({
-      error: "Error checking user",
-    });
-  }
-};
+  const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_SECRET_Expiry,
+  });
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+  };
+
+  res
+    .status(200)
+    .cookie("jwt", token, options)
+    .json(
+      new ApiResponse(200, {
+        user: {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          role: user.role,
+          image: user.image,
+        },
+      }),
+    );
+});
+
+const logout = asyncHandler(async (req, res) => {
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development",
+  };
+
+  return res
+    .status(200)
+    .clearCookie("jwt", options)
+    .json(new ApiResponse(200, {}, "User logged out"));
+});
+
+const getProfile = asyncHandler(async (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, { user: req.user }, "User authenticated"));
+});
 
 export { register, login, logout, getProfile };
