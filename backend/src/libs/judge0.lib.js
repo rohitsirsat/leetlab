3d37d46f7f11086efbaaec57c440a2d45e0300f2
rohit <rohitsirsat855@gmail.com>
2025-07-14 22:26:09 +0530

    add utils

---
+++ backend/src/libs/judge0.lib.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { asyncHandler } from "../utils/async-handler.js";
+
+export const getJudge0LanguageId = (Language) => {
+  const languageMap = {
+    PYTHON: 71,
+    JAVA: 62,
+    JAVASCRIPT: 63,
+  };
+  return languageMap[Language.toUpperCase()];
+};
+
+export const submitBatch = asyncHandler(async (submissions) => {
+  const { data } = await axios.post(
+    `${process.env.JUDGE0_API_URL}/submissions/batch?base64_encoded=false`,
+    { submissions },
+  );
+
+  console.log("Submission Results: ", data);
+
+  return data; // [{token}, {token}, {token}]
+});
+
+const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const pollBatchResults = asyncHandler(async (tokens) => {
+  console.log("Polling batch results");
+  while (true) {
+    const { data } = await axios.get(
+      `${process.env.JUDGE0_API_URL}/submissions/batch`,
+      {
+        params: {
+          tokens: tokens.join(","),
+          base64_encoded: false,
+        },
+      },
+    );
+
+    const results = data.submissions;
+
+    const isAllDone = results.every(
+      (r) => r.status.id !== 1 && r.status.id !== 2,
+    );
+
+    if (isAllDone) return results;
+    await sleep(1000);
+  }
+});
